51919adce5ea1fa62004cc3d276b4cf0ec7e6b6e
Anthony Green <green@moxielogic.com>
2021-12-01 12:23:48 -0500

    Optimize

--- 01.lisp
+++ 01.lisp
@@ -1,34 +1,5 @@
 ;;; Advent Of Code 2021 - Day 1 - Anthony Green <green@moxielogic.com>
 
-(defparameter +numbers+ (mapcar #'parse-integer (uiop:read-file-lines "01.input")))
-
-;; Part 1
-
-(defun up-count (nlist)
-  (if (cdr nlist)
-      (+ (if (< (car nlist) (cadr nlist)) 1 0)
-         (up-count (cdr nlist)))
-      0))
-
-(print (up-count +numbers+))
-
-;; Part 2
-
-(defun up-count-window (nlist)
-  (flet ((window-sum (nlist)
-           (if (caddr nlist)
-               (+ (car nlist) (cadr nlist) (caddr nlist))
-               nil)))
-    (let ((a (window-sum nlist))
-          (b (window-sum (cdr nlist))))
-      (if (and a b)
-          (+ (if (< a b) 1 0)
-             (up-count-window (cdr nlist)))
-          0))))
-
-(print (up-count-window +numbers+))
-
-;; POSTMORTEM:
-;; I used recursion, however I like this answer better:
-;;  https://github.com/henrybw/advent-of-code-2021/blob/main/day1.lisp
-;; It's a reminder that Common Lisp is three languages: Lisp, Loop and Format.
+(let ((input (mapcar #'parse-integer (uiop:read-file-lines "01.input"))))
+  (print (loop for (a b) on input while b count (> b a)))
+  (print (loop for (a b c d) on input while d count (> d a))))
