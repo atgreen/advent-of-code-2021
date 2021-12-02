2c737dfcdd1410def19a4ad50a70367fb7bfd09d
Anthony Green <green@moxielogic.com>
2021-12-02 07:05:37 -0500

    Optimize with reduce

--- 02.lisp
+++ 02.lisp
@@ -8,13 +8,9 @@
 (parseq:defrule value () (rep (0 10) digit) (:string) (:function #'parse-integer))
 (parseq:defrule move () (and (or up down forward) " " value) (:lambda (m i s) (mapcar (lambda (v) (* v s)) m)))
 
-(print (let ((x 0) (y 0))
-         (mapc (lambda (line)
-                 (let ((movement (parseq:parseq 'move line)))
-                   (setf x (+ x (car movement)))
-                   (setf y (+ y (cadr movement)))))
-               (uiop:read-file-lines "02.input"))
-         (* x y)))
+(print (reduce #'* (reduce (lambda (x y) (mapcar #'+ x y))
+                           (mapcar (lambda (line) (parseq:parseq 'move line))
+                                   (uiop:read-file-lines "02.input")))))
 
 (print (let ((x 0) (y 0) (aim 0))
          (mapc (lambda (line)
